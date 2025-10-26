ec880948694ea1252b2d7be93be3fc5cd6a94a83
mescheryakov1 <mescheryakov@me.com>
2025-10-26 15:19:55 +0300

    Fetch wtpkcs11ecp libraries from GitHub release

--- pkcs11.py
+++ pkcs11.py
@@ -5,13 +5,13 @@ from pkcs11_structs import CK_VOID_PTR
 def load_pkcs11_lib():
     """Загружает библиотеку PKCS#11 в зависимости от платформы."""
     if sys.platform.startswith('win'):
-        path = './rtpkcs11ecp.dll'
+        path = './wtpkcs11ecp.dll'
         loader = ctypes.WinDLL  # Используем stdcall для Windows
     elif sys.platform == 'darwin':
-        path = './rtpkcs11ecp.dylib'
+        path = './wtpkcs11ecp.dylib'
         loader = ctypes.CDLL  # Используем cdecl для macOS
     else:
-        path = './librtpkcs11ecp.so'
+        path = './libwtpkcs11ecp.so'
         loader = ctypes.CDLL  # Используем cdecl для Linux
 
     try:
